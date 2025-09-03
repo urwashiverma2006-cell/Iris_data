4d0e4fc213e3184c90a687c60122d65bf58395a4
urwashiverma2006-cell <urwashiverma2006@gmail.com>
2025-09-03 11:54:03 +0530

    Update evaluate.py

--- model/evaluate.py
+++ model/evaluate.py
@@ -4,7 +4,7 @@ from sklearn.metrics import accuracy_score
 import joblib
 
 #Load the dataset
-data=pd.read_csv('data/iris.csv')
+data=pd.read_csv('iris.csv')
 
 #Preprocess the dataset
 X= data.drop('Species',axis=1)
